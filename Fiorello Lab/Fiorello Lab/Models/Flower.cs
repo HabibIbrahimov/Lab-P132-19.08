bf08ad9c247bffcfb283ba233419c12942fd3adc
HabibIbrahimov <habibyi@code.edu.az>
2022-12-03 14:11:28 +0400

    feat: FiorelloLab added

---
+++ Fiorello Lab/Fiorello Lab/Models/Flower.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fiorello_Lab.Models
+{
+    public class Flower
+    {
+        public int Id { get; set; }
+        [MaxLength(80)]
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        [MaxLength(100)]
+        public string? ImgUrl { get; set; }
+        [Required]
+        public int? CategorieId { get; set; }
+
+        public Categorie? Categorie { get; set; }
+
+    }
+}
